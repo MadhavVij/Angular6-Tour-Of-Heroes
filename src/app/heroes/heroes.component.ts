7acf419a81f1a7d4c10cbef2235adfecf2db2da0
Madhav Vij <madhav@infomaticscorp.com>
2018-07-26 14:32:30 -0400

    Chapter 6: Services

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import {HEROES} from '../mock-heroes';
+import {HeroService} from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  selectedHero: Hero;
+  // selectedHero: Hero = {
+  //   id:1,
+  //   name: 'Windstorm'
+  // };
+
+//  heroes = HEROES;
+
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+  
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero:Hero): void{
+    this.selectedHero = hero;
+  }
+
+//  hero = 'Windstorm'
+
+}
