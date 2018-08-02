8225a62cef7e1a9965e6e637827d947dff982e70
Madhav Vij <madhav@infomaticscorp.com>
2018-08-02 10:05:52 -0400

    Completed application

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import {HEROES} from '../mock-heroes';
 import {HeroService} from '../hero.service';
+import { Observable } from '../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-heroes',
@@ -30,6 +31,19 @@ export class HeroesComponent implements OnInit {
   
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if(!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+    .subscribe(hero => {this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h!== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 
   // onSelect(hero:Hero): void{
